f68f8d54cdc2c29a193294913514b6cbf2d79d2a
tahir-ali30 <101398390+tahir-ali30@users.noreply.github.com>
2022-12-01 20:09:43 +0500

    changed GUEST to AWAY

--- index.js
+++ index.js
@@ -8,32 +8,12 @@ let gScoreEl = document.getElementById("guestScore")
 let hScore = 0
 let gScore = 0
 
-function hplus1() {
-    hScore += 1
+function hplusX(x) {
+    hScore += x
     hScoreEl.textContent = hScore
 }
 
-function hplus2() {
-    hScore += 2
-    hScoreEl.textContent = hScore
-}
-
-function hplus3() {
-    hScore += 3
-    hScoreEl.textContent = hScore
-}
-
-function gplus1() {
-    gScore += 1
-    gScoreEl.textContent = gScore
-}
-
-function gplus2() {
-    gScore += 2
-    gScoreEl.textContent = gScore
-}
-
-function gplus3() {
-    gScore += 3
+function gplusX(x) {
+    gScore += x
     gScoreEl.textContent = gScore
 }
